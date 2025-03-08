5e784751c645387a9b1bcbe33dc2dbd8bc5a9478
Rushit Harsora <rushitharsora@gmail.com>
2025-03-08 18:36:49 +0530

    Created Some Components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
-import { Button } from './components/ui/button'
+import Login from './pages/Login'
 
 function App() {
 
 
   return (
-    <Button>login</Button>
+    <main>
+      <Login />
+    </main>
   )
 }
 
